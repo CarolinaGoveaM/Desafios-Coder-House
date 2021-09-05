5a5d62d4ffd94963b686307e838480ef03bfda2c
carolina <dennysgovea@gmail.com>
2021-09-04 21:03:02 -0300

    subida al repositorio

---
+++ Desafio 1/main.js
@@ -0,0 +1,10 @@
+let nombre = prompt ("Ingrese su primer Nombre");
+let apellido = prompt ("Ingrese su primer Apellido");
+let edad = prompt ("Ingrese su Edad");
+let edadEnNumero = parseInt(edad);
+
+let edadEnDosAños = edadEnNumero + 2;
+
+alert ("Hola " + nombre + " " + apellido);
+alert ("Tienes " + edad + " " + "años")
+alert ("En dos años tendrás " + edadEnDosAños);
