c35ba408359ea2f88529abb5abb5067946f38f6a
carolina <dennysgovea@gmail.com>
2021-09-04 21:13:36 -0300

    modificacion de variable

--- Desafio 1/main.js
+++ Desafio 1/main.js
@@ -2,8 +2,9 @@ let nombre = prompt ("Ingrese su primer Nombre");
 let apellido = prompt ("Ingrese su primer Apellido");
 let edad = prompt ("Ingrese su Edad");
 let edadEnNumero = parseInt(edad);
+let numero = 2
 
-let edadEnDosAños = edadEnNumero + 2;
+let edadEnDosAños = edadEnNumero + numero;
 
 alert ("Hola " + nombre + " " + apellido);
 alert ("Tienes " + edad + " " + "años")
